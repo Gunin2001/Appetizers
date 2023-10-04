5b67e9689c0ec16ef9b42268fa97723b6ff89f3f
Gunin2001 <gunin2001@gmail.com>
2023-10-04 17:04:31 +0530

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Gunin Gupta on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
